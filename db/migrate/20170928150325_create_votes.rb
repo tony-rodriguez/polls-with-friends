a0860048d23ddb7b50e5324c4a637f70d0466a1d
Tony Rodriguez <tonyr923@gmail.com>
2017-09-28 13:31:08 -0500

    Add null false constraints for votes

--- db/migrate/20170928150325_create_votes.rb
+++ db/migrate/20170928150325_create_votes.rb
@@ -1,8 +1,8 @@
 class CreateVotes < ActiveRecord::Migration[5.1]
   def change
     create_table :votes do |t|
-      t.integer :voter_id
-      t.integer :response_id
+      t.integer :voter_id, null: false
+      t.integer :response_id, null: false
 
       t.timestamps
     end
