48ddd554d66319ef51d511b8b1cf5bcf58377f52
Tony Rodriguez <tonyr923@gmail.com>
2017-09-28 16:37:00 -0500

    Add form and controller methods for user registration

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,9 +6,10 @@ class UsersController < ApplicationController
 
   def create
     @user = User.new(user_params)
+
     if @user.save
       session[:user_id] = @user.id
-      redirect
+      redirect_to '/'
     else
       render 'new'
     end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
   has_secure_password
-  validates :username, uniqueness: true, presence: true
+  validates_uniqueness_of :username
+  validates_presence_of :username
+
 end
